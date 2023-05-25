2346ad5f08c58b21dd6a1aae5157aa7f1a88ec18
abrarimamsatria09 <092004abrar@gmail.com>
2023-05-25 11:44:49 +0700

    digunakan untuk menampilkan data dalam linked list berdasarkan nomor secara terurut dari yang terbesar ke yang terkecil

--- Double Linked list_042/Double Linked list_042.cpp
+++ Double Linked list_042/Double Linked list_042.cpp
@@ -21,7 +21,7 @@ public:
 	bool deleteNode(int rollNo);
 	bool listEmpty();
 	void ascending();
-	void discending();
+	void descending();
 	void hapus();
 	void searchData();
 };
@@ -110,6 +110,22 @@ void DoubleLinkedList::ascending() {
 		while (currentNode != NULL) {
 			cout << currentNode->noMhs << " " << currentNode->name << endl;
 			currentNode = currentNode->next;
+		}
+	}
+}
+
+void DoubleLinkedList::descending() {
+	if (listEmpty())
+		cout << "\nList is empty" << endl;
+	else {
+		cout << "\nRecords in descending order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL)
+			currentNode = currentNode->next;
+
+		while (currentNode != NULL) {
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->prev;
 		}
 	}
 }
