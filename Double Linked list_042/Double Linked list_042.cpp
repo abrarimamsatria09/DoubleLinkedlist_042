f85a140b964f69e3eaa0a8aa32eba9ccf0d09dee
abrarimamsatria09 <092004abrar@gmail.com>
2023-05-25 16:47:53 +0700

    digunakan untuk menjalankan fungsi yang sesuai berdasarkan pilihan pengguna yang diambil dari variabel ch.

--- Double Linked list_042/Double Linked list_042.cpp
+++ Double Linked list_042/Double Linked list_042.cpp
@@ -177,6 +177,26 @@ int main() {
 			cout << "\nEnter your choice (1-6): ";
 			char ch;
 			cin >> ch;
+
+			switch (ch) {
+			case '1':
+				obj.addNode();
+				break;
+			case '2':
+				obj.hapus();
+				break;
+			case '3':
+				obj.ascending();
+				break;
+			case '4':
+				obj.descending();
+				break;
+			case '5':
+				obj.searchData();
+				break;
+			case '6':
+				return 0;
+			}
 		}
 	}
 }
