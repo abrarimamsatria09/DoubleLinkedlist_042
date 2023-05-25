8c801e9549c1a7a292d4ba5482a8ff548762ce75
abrarimamsatria09 <092004abrar@gmail.com>
2023-05-25 11:52:25 +0700

    untuk mengahapus nomor student tertentu dari linked list

--- Double Linked list_042/Double Linked list_042.cpp
+++ Double Linked list_042/Double Linked list_042.cpp
@@ -135,4 +135,12 @@ void DoubleLinkedList::hapus() {
 		cout << "\nList is empty" << endl;
 
 	}
+	cout << "\mEnter the roll number of the student whose record is to be deleted: ";
+	int rollNo;
+	cin >> rollNo;
+	cout << endl;
+	if (DoubleLinkedList::deleteNode(rollNo) == false)
+		cout << "Record not found" << endl;
+	else
+		cout << "Record with roll number" << rollNo << " deleted" << endl;
 }
